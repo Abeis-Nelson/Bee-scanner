18718ea17bc3d310a13ff6a09b9dbbfe40c29ca4
Abeis-Nelson <abeis1728@gmail.com>
2021-12-26 19:00:46 +0530

    change name

---
+++ bee_scanner.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+       
+cat banner.txt
+echo \n
+
+
+if [ "$1" == "" ]
+then
+echo "Please prodvide the necessary arguments to let the byakugon use its power in your network"
+echo "Usage Example: ./byakugon.sh 192.168.1."
+else
+for ip in `seq 1 255`; do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | sed "s/.$//" &
+done
+fi
