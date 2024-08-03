df2f79c86785a3966228ecbbca45bc0488d3917b
Abeis Nelson <abeis1728@gmail.com>
2024-08-04 03:44:43 +0530

    Update bee_scanner.sh

--- bee_scanner.sh
+++ bee_scanner.sh
@@ -8,7 +8,7 @@ echo \n
 if [ "$1" == "" ]
 then
 echo "Please prodvide the necessary arguments to let the byakugon use its power in your network"
-echo "Usage Example: ./byakugon.sh 192.168.1."
+echo "Usage Example: ./bee_scanner.sh 192.168.1."
 else
 for ip in `seq 1 255`; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | sed "s/.$//" &
